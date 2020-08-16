f30c5e88326e8ba32626fd7d0ba13b5c2967e719
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2020-08-16 15:14:06 -0700

    starting scraper

--- scrape.py
+++ scrape.py
@@ -1,3 +1,20 @@
 """Scrape
 Scrape tweets from a given username
 """
+
+# Modified from https://github.com/minimaxir/download-tweets-ai-text-gen
+
+import os
+import logging
+import csv
+import re
+import twint
+import fire
+from datetime import datetime as dt
+from time import sleep
+from tqdm import tqdm
+
+
+# Disable logging to get rid of any warnings
+logger = logging.getLogger()
+logger.disabled = True
